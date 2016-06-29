530c819d67d0c972da674e2835a1ea126c985238
mattclarke <matt.clarke@stfc.ac.uk>
2016-06-29 09:01:01 +0100

    Corrected indentation

--- streamer/startup.py
+++ streamer/startup.py
@@ -10,11 +10,11 @@ def start_streamer_daemon_threads(base_generator, parameter_controller, kafka_br
     
     if kafka_broker is None:
 	# Default to zmq
-	from fake_streamer import FakeEventStreamer
-	streamer = FakeEventStreamer(event_generator)
+	    from fake_streamer import FakeEventStreamer
+	    streamer = FakeEventStreamer(event_generator)
     else:
         from fake_streamer_kafka import FakeEventStreamer
-	streamer = FakeEventStreamer(event_generator, kafka_broker)
+	    streamer = FakeEventStreamer(event_generator, kafka_broker)
     
     streamer_thread = Thread(target=streamer.run)
     streamer_thread.daemon = True
